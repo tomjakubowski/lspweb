45e62f50f13622c50525c5fc093bb1fbb604db78
Tom Jakubowski <tom@crystae.net>
2019-10-02 23:45:14 -0700

    initialize language server

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
-use crossbeam::channel as chan;
 use lsp_types;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use serde_json;
 use std::{
-    io,
-    path::Path,
+    io::{self, BufReader},
     process::{self, Command, Stdio},
     sync::{Arc, Mutex},
 };
@@ -13,17 +11,45 @@ use warp::{self, Filter, Reply};
 type Lsp = Arc<Mutex<LsProcess>>;
 
 fn main() {
+    env_logger::init();
     // Initialize language server process
     let lsp = Arc::new(Mutex::new(
         LsProcess::start().expect("Language server failed to start"),
     ));
-    let lsp_filter = warp::any().map(move || lsp.clone());
 
-    let app = warp::path("debug")
-        .and(lsp_filter)
+    let lsp_filter = warp::any().map(move || lsp.clone());
+    let debug_route = lsp_filter
+        .and(warp::path("debug"))
         .and(warp::get2())
         .map(handle_debug);
-    warp::serve(app).run(([127, 0, 0, 1], 8100));
+    let routes = debug_route; // or...
+    const PORT: u16 = 8100;
+    let allowed_hosts = [
+        "127.0.0.1".to_string(),
+        "localhost".to_string(),
+        format!("127.0.0.1:{}", PORT),
+        format!("localhost:{}", PORT),
+    ];
+    let app = host_filter(move |host: &str| allowed_hosts.iter().any(|h| h == host)).and(routes);
+
+    log::info!("Serving");
+    warp::serve(app).run(([127, 0, 0, 1], PORT));
+}
+
+fn host_filter<'a, F>(hostp: F) -> warp::filters::BoxedFilter<()>
+where
+    F: Fn(&str) -> bool + Clone + Send + Sync + 'static,
+{
+    warp::header("Host")
+        .and_then(move |req_host: String| {
+            if hostp(&req_host) {
+                Ok(())
+            } else {
+                Err(warp::reject::custom("Invalid Host header"))
+            }
+        })
+        .untuple_one()
+        .boxed()
 }
 
 fn handle_debug(lsp: Lsp) -> impl Reply {
@@ -49,37 +75,62 @@ fn handle_debug(lsp: Lsp) -> impl Reply {
 
 #[derive(Debug)]
 struct LsProcess {
+    buf_stdout: BufReader<process::ChildStdout>,
+    stdin: process::ChildStdin,
     proc: process::Child,
     seqnum: u64,
 }
 
 impl LsProcess {
     pub fn start() -> io::Result<LsProcess> {
-        let mut proc = Command::new("ra_lsp_server")
-            .stdin(Stdio::piped())
-            .stderr(Stdio::inherit())
-            .stdout(Stdio::piped())
-            .spawn()?;
-        let mut lsp = LsProcess { proc, seqnum: 0 };
+        let mut lsp: LsProcess = {
+            let mut proc = Command::new("ra_lsp_server")
+                .stdin(Stdio::piped())
+                .stderr(Stdio::inherit())
+                .stdout(Stdio::piped())
+                .spawn()?;
+            let stdin = proc.stdin.take().unwrap();
+            let buf_stdout = BufReader::new(proc.stdout.take().unwrap());
+            LsProcess {
+                buf_stdout,
+                stdin,
+                proc,
+                seqnum: 0,
+            }
+        };
 
         let cwd = std::env::current_dir().unwrap();
         let workspace_dir: &str = cwd.to_str().unwrap();
         let init_req = RpcRequest {
             id: lsp.seqnum,
             method: "initialize",
-            params: serde_json::to_value(initialize_params(workspace_dir)).unwrap(),
+            params: initialize_params(workspace_dir),
         };
         lsp.seqnum += 1;
+
         let init_json = serde_json::to_value(init_req).unwrap();
         use std::io::Write;
-        let stdin = lsp.proc.stdin.as_mut().unwrap();
-        write!(stdin, "{}", wrap_ls_request(&init_json).unwrap()).unwrap();
+        let stdin = &mut lsp.stdin;
+        let req = wrap_ls_request(&init_json).unwrap();
+        log::trace!("writing request:\n{}", req);
+        write!(stdin, "{}", req)?;
         stdin.flush().unwrap();
+
+        let mut read_buf = String::new();
+        use std::io::{BufRead, Read};
+        lsp.buf_stdout.read_line(&mut read_buf)?;
+        let content_len = parse_content_length(&read_buf).expect("Content-Length parse failed");
+        lsp.buf_stdout.read_line(&mut read_buf)?;
+        let mut read_buf = vec![0u8; content_len];
+        lsp.buf_stdout.read_exact(&mut read_buf)?;
+        let res: RpcResponse = serde_json::from_slice(&read_buf).unwrap();
+        log::trace!("RpcResponse:\n\n{:#?}", res);
+        // log::trace!("Json:\n\n{}", String::from_utf8(read_buf).unwrap());
 
         Ok(lsp)
     }
 
-    fn write_request(&mut self, method: &'static str, params: serde_json::Value) {}
+    // fn write_request(&mut self, method: &'static str, params: serde_json::Value) {}
 
     pub fn pid(&self) -> u32 {
         self.proc.id()
@@ -92,6 +143,20 @@ impl Drop for LsProcess {
     }
 }
 
+fn parse_content_length(line: &str) -> Option<usize> {
+    let line = line.trim_end();
+    let needle = "Content-Length: ";
+    let mid = line.find(needle)? + needle.len();
+    let (_, num) = line.split_at(mid);
+    println!("num: {}", num);
+    num.parse().ok()
+}
+
+#[test]
+fn test_parse_content_length() {
+    assert_eq!(37, parse_content_length("Content-Length: 37").unwrap());
+}
+
 #[derive(Debug, Serialize)]
 struct RpcRequest {
     id: u64,
@@ -99,11 +164,66 @@ struct RpcRequest {
     params: serde_json::Value,
 }
 
-fn initialize_params(workspace_path: &str) -> lsp_types::InitializeParams {
+#[derive(Debug, Deserialize, PartialEq)]
+struct RpcResponse {
+    id: Option<u64>,
+    #[serde(flatten)]
+    payload: ResponsePayload,
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+enum ResponsePayload {
+    #[serde(rename = "result")]
+    Result(serde_json::Value),
+    #[serde(rename = "error")]
+    Error(RpcError),
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+struct RpcError {
+    code: i64,
+    message: String,
+}
+
+#[test]
+fn test_deserialize_rpc_response() {
+    let json = r#"{
+      "jsonrpc": "2.0",
+      "id": 69,
+      "result": "nice"
+    }"#;
+    assert_eq!(
+        RpcResponse {
+            id: Some(69),
+            payload: ResponsePayload::Result(serde_json::Value::String("nice".to_string()))
+        },
+        serde_json::from_str(json).unwrap()
+    );
+    let json = r#"{
+      "jsonrpc": "2.0",
+      "id": 69,
+      "error": {
+        "code": -420,
+        "message": "not nice"
+      }
+    }"#;
+    assert_eq!(
+        RpcResponse {
+            id: Some(69),
+            payload: ResponsePayload::Error(RpcError {
+                code: -420,
+                message: "not nice".to_string()
+            })
+        },
+        serde_json::from_str(json).unwrap()
+    );
+}
+
+fn initialize_params(workspace_path: &str) -> serde_json::Value {
     use lsp_types::{ClientCapabilities, InitializeParams, TraceOption, Url};
     let mut url = Url::parse("file:///").unwrap();
     url.set_path(workspace_path);
-    InitializeParams {
+    serde_json::to_value(InitializeParams {
         process_id: Some(std::process::id() as u64),
         root_path: Some(workspace_path.to_string()),
         root_uri: Some(url),
@@ -111,12 +231,13 @@ fn initialize_params(workspace_path: &str) -> lsp_types::InitializeParams {
         capabilities: ClientCapabilities::default(),
         trace: Some(TraceOption::Verbose),
         workspace_folders: None,
-    }
+    })
+    .unwrap()
 }
 
 fn wrap_ls_request(val: &serde_json::Value) -> Result<String, serde_json::Error> {
     let json = serde_json::to_string(&val)?;
-    Ok(format!("Content-Length: {}\r\n{}", json.len(), json))
+    Ok(format!("Content-Length: {}\r\n\r\n{}", json.len(), json))
 }
 
 // #[derive(Debug)]
